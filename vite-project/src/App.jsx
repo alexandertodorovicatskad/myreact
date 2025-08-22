a50d0aff9a3151b3656fc3c7f51f7f0c04fd3bfd
Alexander Todorovic <130694260+alexandertodorovicatskad@users.noreply.github.com>
2025-08-22 20:54:27 +0000

    Initialize Vite project with React setup, add player component, and style the application

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+import Player from './components/Player.jsx';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player name="Player 1" symbol='X'/>
+          <Player name="Player 2" symbol='O'/>
+        </ol>
+        GAME BOARD
+      </div>
+      LOG
+    </main>
+  );
+}
+
+export default App;
